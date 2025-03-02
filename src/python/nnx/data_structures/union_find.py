395a76f17ab83501b9830e6a6b45490464297911
Alexander Dann <mail@alexanderdann.de>
2025-03-02 15:04:02 +0100

    Adding better union find algorithm

--- src/python/nnx/data_structures/union_find.py
+++ src/python/nnx/data_structures/union_find.py
@@ -50,3 +50,63 @@ class NaiveUnionFind:
                 if self._verbose:
                     pprint(f"Updating {idx} -> {entry2}")
                 self._lookup[idx] = entry2
+
+
+class BetterUnionFind:
+    """Resembles the Weighted Quick Union with Path Compression."""
+
+    def __init__(self, num_elements: int, *, verbose: bool = False) -> None:
+        """C'tor of NaiveUnionFind.
+
+        Args:
+            num_elements: count of components/nodes.
+            verbose: whether to use the print statements
+
+        """
+        self._lookup = list(range(num_elements))
+        self._roots = list(range(num_elements))
+        self._sizes = [1 for _ in self._lookup]
+
+        self._verbose = verbose
+        if self._verbose:
+            pprint(f"Starting point is the following {self._lookup}")
+
+    def connected(self, id1: int, id2: int) -> bool:
+        """Check if two components are connected.
+
+        Args:
+            id1: if of the first node
+            id2: id of the second node
+
+        Returns:
+            True if the two components are connected and false otherwise.
+
+        """
+        return self._root(id1) == self._root(id2)
+
+    def _root(self, idx: int) -> int:
+        while self._lookup[idx] != idx:
+            self._lookup[idx] = self._lookup[self._lookup[idx]]
+            idx = self._lookup[idx]
+
+        return idx
+
+    def union(self, id1: int, id2: int) -> None:
+        """Merge two nodes at given indeces.
+
+        Args:
+            id1: if of the first node
+            id2: id of the second node
+
+        """
+        root1 = self._root(id1)
+        root2 = self._root(id2)
+        if root1 == root2:
+            return
+
+        if self._sizes[root1] < self._sizes[root2]:
+            self._lookup[root1] = root2
+            self._sizes[root2] += self._sizes[root1]
+        else:
+            self._lookup[root2] = root1
+            self._sizes[root1] += self._sizes[root2]
